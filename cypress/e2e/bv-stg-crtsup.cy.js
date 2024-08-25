1e0e40ce322806feb6185447c7256d2d68b9680c
Erika Maria Tavera Misas <dugarerika@gmail.com>
2024-08-25 17:56:56 +0400

    adding test cases for suppliers

--- cypress/e2e/bv-stg-crtsup.cy.js
+++ cypress/e2e/bv-stg-crtsup.cy.js
@@ -45,8 +45,8 @@ const filloutSupplierForm = (sup_name, sup_description, sup_first_name, sup_last
   cy.contains('label>span', 'Contact Last Name').parent().next('div').find('input').type(sup_last_name)
   cy.contains('label>span', 'Contact Mobile').should('exist')
   cy.contains('label>span', 'Contact Mobile').parent().next('div').find('input').type(sup_contact_mobile)
-  cy.contains('label>span', 'Contact Email').should('exist')
-  cy.contains('label>span', 'Contact Email').parent().next('div').find('input').type(sup_contact_email)
+  cy.contains('label>span', 'Contact email').should('exist')
+  cy.contains('label>span', 'Contact email').parent().next('div').find('input').type(sup_contact_email)
 }
 
 const expectedMessageCreateSupplier = (supplier_message) => {
@@ -66,7 +66,7 @@ describe.only('Beta Vendor Admin | Inventory | Create Suppliers|logged with Admi
     cy.clearCookies()
   })
 
-  it('Verify it is possible access to the Inventory/Suplliers section- Admin credentials', () => {
+  it.only('Verify it is possible access to the Inventory/Suplliers section- Admin credentials', () => {
     cy.visit('https://beta.vendor.bookr-dev.com/admin/calendar')
     cy.contains('Inventory').should('exist')
     cy.contains('Inventory').click({ force: true })
@@ -75,43 +75,68 @@ describe.only('Beta Vendor Admin | Inventory | Create Suppliers|logged with Admi
     cy.contains('h6', 'Suppliers').should('exist')
   })
 
-  it('Verify the it is possible access to the Create Suppliers form - Admin credentials', () => {
+  it.only('Verify the it is possible access to the Create Suppliers form - Admin credentials', () => {
     accessToCreateSuppliers()
   })
 
   //Unsuccessfully suplier creation
-  it('Verify suppliers Name is the required field by trying to add new supplier leaving empty all the fields- Admin credentials', () => {
+  it.only('Verify suppliers Name is the required field by trying to add new supplier leaving empty all the fields- Admin credentials', () => {
     accessToCreateSuppliers()
     filloutSupplierForm('{enter}','{enter}','{enter}','{enter}','{enter}','{enter}')
-    expectedMessageCreateProduct('Supplier name is required')
+    expectedMessageCreateSupplier('Supplier name is required')
   })
 
-  it('Verify suppliers Name is the required field by trying to add new supplier fiiling only the supplier Description- Admin credentials', () => {
+  it.only('Verify suppliers Name is the required field by trying to add new supplier fiiling only the supplier Description- Admin credentials', () => {
     accessToCreateSuppliers()
     filloutSupplierForm('{enter}','Supplier Description','{enter}','{enter}','{enter}','{enter}')
-    expectedMessageCreateSupplier('Supplier created successfully')
+    expectedMessageCreateSupplier('Supplier name is required')
   })
 
-  it('Verify suppliers Name is the required field by trying to add new supplier fiiling only the Contact Firstname- Admin credentials', () => {
+  it.only('Verify suppliers Name is the required field by trying to add new supplier fiiling only the Contact First Name- Admin credentials', () => {
     accessToCreateSuppliers()
     filloutSupplierForm('{enter}','{enter}','contact first name','{enter}','{enter}','{enter}')
-    expectedMessageCreateSupplier('Supplier created successfully')
+    expectedMessageCreateSupplier('Supplier name is required')
   })
 
-  it('Verify suppliers Name is the required field by trying to add new supplier fiiling only the Contact Firstname- Admin credentials', () => {
+  it.only('Verify suppliers Name is the required field by trying to add new supplier fiiling only the Contact Last Name- Admin credentials', () => {
     accessToCreateSuppliers()
-    filloutSupplierForm('{enter}','{enter}','contact first name','{enter}','{enter}','{enter}')
+    filloutSupplierForm('{enter}','{enter}','{enter}','contact last name','{enter}','{enter}')
+    expectedMessageCreateSupplier('Supplier name is required')
+  })
+  
+  it.only('Verify suppliers Name is the required field by trying to add new supplier fiiling only the Contact Mobile- Admin credentials', () => {
+    accessToCreateSuppliers()
+    filloutSupplierForm('{enter}','{enter}','{enter}','{enter}','581199141','{enter}')
+    expectedMessageCreateSupplier('Supplier name is required')
+  })
+
+  
+  it.only('Verify suppliers Name is the required field by trying to add new supplier fiiling only email- Admin credentials', () => {
+    accessToCreateSuppliers()
+    filloutSupplierForm('{enter}','{enter}','{enter}','{enter}','{enter}','tests@gmail.com')
+    expectedMessageCreateSupplier('Supplier name is required')
+  })
+
+  it.only('Verify suppliers Name is the required field by trying to add new supplier fiiling only the Contact Mobile and email- Admin credentials', () => {
+    accessToCreateSuppliers()
+    filloutSupplierForm('{enter}','{enter}','{enter}','{enter}','581199141','tests@gmail.com')
+    expectedMessageCreateSupplier('Supplier name is required')
+  })
+
+  it.only('Verify suppliers Name is the required field by trying to add new supplier fiiling all the form fiels exepct Supplliers name- Admin credentials', () => {
+    accessToCreateSuppliers()
+    filloutSupplierForm('{enter}','Suppliers description','contact fisrt name','contact last name','581199141','tests@gmail.com')
+    expectedMessageCreateSupplier('Supplier name is required')
+  })
+
+  //Successfully suplier creation
+  it.only('Verify suppliers Name is the required field by trying to add new supplier fiiling only the supplier name- Admin credentials', () => {
+    accessToCreateSuppliers()
+    filloutSupplierForm('Supplier with all the fields fillout','Supplier Description','contact First Name','Contact Last Name','581199141','3@gmail.com')
     expectedMessageCreateSupplier('Supplier created successfully')
   })
   
-    //Successfully suplier creation
-  it('Verify suppliers Name is the required field by trying to add new supplier fiiling only the supplier name- Admin credentials', () => {
-    accessToCreateSuppliers()
-    filloutSupplierForm('Supplier with all the fields fillout','Supplier Description','contact First Name','Contact Last Name','COntract mobile','3@gmail.com')
-    expectedMessageCreateSupplier('Supplier created successfully')
-  })
-  
-  it('Verify suppliers Name is the required field by trying to add new supplier fiiling only the supplier name- Admin credentials', () => {
+  it.only('Verify suppliers Name is the required field by trying to add new supplier fiiling only the supplier name- Admin credentials', () => {
     accessToCreateSuppliers()
     filloutSupplierForm('Supplier with all the fields fillout','Supplier Description','contact First Name','Contact Last Name','COntract mobile','3@gmail.com')
     expectedMessageCreateSupplier('Supplier created successfully')
@@ -119,7 +144,7 @@ describe.only('Beta Vendor Admin | Inventory | Create Suppliers|logged with Admi
 
   //Delete suppliers
 
-  it('Verify it is possible delete suplliers from the Inventory/Supplier list section- Admin credentials', () => {
+  it.only('Verify it is possible delete suplliers from the Inventory/Supplier list section- Admin credentials', () => {
     cy.visit('https://beta.vendor.bookr-dev.com/admin/calendar')
     cy.contains('Inventory').should('exist')
     cy.contains('Inventory').click({ force: true })
@@ -134,77 +159,6 @@ describe.only('Beta Vendor Admin | Inventory | Create Suppliers|logged with Admi
     cy.contains('p', 'Are you sure you want to delete this supplier?').should('exist')
     cy.contains('p', 'Are you sure you want to delete this supplier?').parents('section').next('div').find('button').eq(1).click({ force: true })
     cy.contains('span', 'Supplier deleted successfully').should('exist')
-    cy.get('tbody').find('tr').its('length').then(count => {
-      if (count) {   // not count >= 0, because 0 means no elements
-        cy.log(`there are ${count - 1} elements`)
-      }
-    })
-  })
-
-  it.skip('Verify it is possible delete suplliers from the Inventory/Supplier list section- Admin credentials', () => {
-    cy.visit('https://beta.vendor.bookr-dev.com/admin/calendar')
-    cy.contains('Inventory').should('exist')
-    cy.contains('Inventory').click({ force: true })
-    cy.contains('Supplier').should('exist')
-    cy.contains('Supplier').click({ force: true })
-    cy.contains('h6', 'Supplier').should('exist')
-    cy.get('tbody>*').should('exist')
-    cy.get('tbody>*').first().click({ force: true })
-    cy.contains('h3', 'Supplier Details').should('exist')
-    cy.contains('button', 'Delete').should('exist')
-    cy.contains('button', 'Delete').click({ force: true })
-    cy.contains('p', 'Are you sure you want to delete this supplier?').should('exist')
-    cy.contains('p', 'Are you sure you want to delete this supplier?').parents('section').next('div').find('button').eq(1).click({ force: true })
-    cy.contains('span', 'Supplier deleted successfully').should('exist')
-    cy.get('tbody').find('tr').its('length').then(count => {
-      if (count) {   // not count >= 0, because 0 means no elements
-        cy.log(`there are ${count - 1} elements`)
-      }
-    })
-  })
-
-  it.skip('Verify it is possible delete suplliers from the Inventory/Supplier list section- Admin credentials', () => {
-    cy.visit('https://beta.vendor.bookr-dev.com/admin/calendar')
-    cy.contains('Inventory').should('exist')
-    cy.contains('Inventory').click({ force: true })
-    cy.contains('Supplier').should('exist')
-    cy.contains('Supplier').click({ force: true })
-    cy.contains('h6', 'Supplier').should('exist')
-    cy.get('tbody>*').should('exist')
-    cy.get('tbody>*').first().click({ force: true })
-    cy.contains('h3', 'Supplier Details').should('exist')
-    cy.contains('button', 'Delete').should('exist')
-    cy.contains('button', 'Delete').click({ force: true })
-    cy.contains('p', 'Are you sure you want to delete this supplier?').should('exist')
-    cy.contains('p', 'Are you sure you want to delete this supplier?').parents('section').next('div').find('button').eq(1).click({ force: true })
-    cy.contains('span', 'Supplier deleted successfully').should('exist')
-    cy.get('tbody').find('tr').its('length').then(count => {
-      if (count) {   // not count >= 0, because 0 means no elements
-        cy.log(`there are ${count - 1} elements`)
-      }
-    })
-  })
-
-  it.skip('Verify it is possible delete suplliers from the Inventory/Supplier list section- Admin credentials', () => {
-    cy.visit('https://beta.vendor.bookr-dev.com/admin/calendar')
-    cy.contains('Inventory').should('exist')
-    cy.contains('Inventory').click({ force: true })
-    cy.contains('Supplier').should('exist')
-    cy.contains('Supplier').click({ force: true })
-    cy.contains('h6', 'Supplier').should('exist')
-    cy.get('tbody>*').should('exist')
-    cy.get('tbody>*').first().click({ force: true })
-    cy.contains('h3', 'Supplier Details').should('exist')
-    cy.contains('button', 'Delete').should('exist')
-    cy.contains('button', 'Delete').click({ force: true })
-    cy.contains('p', 'Are you sure you want to delete this supplier?').should('exist')
-    cy.contains('p', 'Are you sure you want to delete this supplier?').parents('section').next('div').find('button').eq(1).click({ force: true })
-    cy.contains('span', 'Supplier deleted successfully').should('exist')
-    cy.get('tbody').find('tr').its('length').then(count => {
-      if (count) {   // not count >= 0, because 0 means no elements
-        cy.log(`there are ${count - 1} elements`)
-      }
-    })
   })
 
   })
