fcba093c981ab7195dc025affc8591fba7d686f4
Erika Dugar <dugarerika@gmail.com>
2024-10-26 14:15:44 +0300

    fixing URLs after grouping test cases by environtment

--- cypress/e2e/staging/inventory/bv-stg-crtsup.cy.js
+++ cypress/e2e/staging/inventory/bv-stg-crtsup.cy.js
@@ -3,23 +3,6 @@
 
 const { should } = require("chai")
 
-const login = (name, username, password) => {
-  cy.session(name, () => {
-    cy.visit('https://beta.vendor.bookr-dev.com/')
-    cy.url().should('include', 'https://beta.vendor.bookr-dev.com/auth')
-    cy.get('[type="text"]').should('be.visible')
-    cy.get('[type="password"]').should('be.visible')
-    cy.xpath('//button[text()="Login"]').should('be.visible')
-    cy.get('[type="text"]').type(username, { force: true, delay: 50 })
-    cy.get('[type="password"]').type(password, { force: true, delay: 50 })
-    cy.intercept('POST', '/api/main/auth/login').as('sign')
-    cy.xpath('//button[text()="Login"]').click()
-    cy.wait('@sign').then((interception) => {
-      expect(interception.response.statusCode).to.equal(200)
-    })
-  })
-}
-
 const accessToCreateSuppliers = () => {
   cy.visit('https://beta.vendor.bookr-dev.com/admin/calendar')
   cy.contains('Inventory').should('exist')
@@ -59,7 +42,7 @@ const expectedMessageCreateSupplier = (supplier_message) => {
 describe('Beta Vendor Admin | Inventory | Create Suppliers|logged with Admin credentials', () =>{
 
   beforeEach(() => {
-    login('Admin Section', 'artnailcorner', '1234567890')
+    cy.login('Admin Section', 'artnailcorner', '1234567890')
   })
 
   afterEach(() => {
