6dd5737ee089fded36ebaf6d77a2225f04c5c821
Erika Dugar <dugarerika@gmail.com>
2024-11-05 10:00:57 +0300

    fixing create produc form

--- cypress/e2e/production/inventory/bv-prod-crtprod.cy.js
+++ cypress/e2e/production/inventory/bv-prod-crtprod.cy.js
@@ -8,7 +8,7 @@ const { should } = require("chai")
 describe('Production - Beta Vendor Admin | Inventory | Create products| logged with Admin credentials', () => {
 
   beforeEach(() => {
-    cy.loginprod('Admin Section', Cypress.env("Vendor_Admin_Username_Staging"), Cypress.env("Vendor_Admin_Password_Staging"))
+    cy.loginprod('Admin Section', Cypress.env("Vendor_Admin_Username_Production"), Cypress.env("Vendor_Admin_Password_Production"))
   })
 
   afterEach(() => {
