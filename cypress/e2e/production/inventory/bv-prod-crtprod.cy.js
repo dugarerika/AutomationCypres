224b0688ab979b53d1eb0c2d8ac7807ee4690714
Erika Dugar <dugarerika@gmail.com>
2025-01-09 10:08:07 +0300

    fixing inventory create product

--- cypress/e2e/production/inventory/bv-prod-crtprod.cy.js
+++ cypress/e2e/production/inventory/bv-prod-crtprod.cy.js
@@ -122,7 +122,7 @@ describe('Production - Beta Vendor Admin | Inventory | Create products| logged w
     cy.contains('h3', 'Brands').should('exist')
     cy.wait(2000)
     cy.get('section').next('div').find('button').click({ force: true })
-    cy.contains('span','Brand').parent().next('div').find('input').type('Automated Brand', { force: true, delay: 50 })
+    cy.contains('span','Brand name').parent().next('div').find('input').type('Automated Brand', { force: true, delay: 50 })
     cy.get('section').next('div').find('button').click({ force: true })
     cy.contains('span', 'Brand created').should('exist')
     cy.contains('label>span', 'Category').parents('label').next('div').find('input').click({ force: true })
