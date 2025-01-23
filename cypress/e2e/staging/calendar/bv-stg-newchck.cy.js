979bcd457425c6b8cc815257e3bef9c1ffdeadbb
Erika Maria Tavera Misas <dugarerika@gmail.com>
2025-01-24 01:01:10 +0400

    adding test cases for New checkout

--- cypress/e2e/staging/calendar/bv-stg-newchck.cy.js
+++ cypress/e2e/staging/calendar/bv-stg-newchck.cy.js
@@ -26,12 +26,17 @@ describe('Staging - Beta Vendor Admin | Calendar| Create Blocktime on the Calend
         cy.clearCookies()
     })
 
-    it('Checkout a service  - Admin credentials', () => {
+    it.only('Checkout a service  - Admin credentials', () => {
         newCheckout()
         cy.contains('div','Search customer..').should('be.visible')
         cy.contains('button','Add New').should('be.visible')
         cy.contains('button','Add New').click({force: true})
         cy.get('div[role="tablist"]').find('button').eq(0).click({force: true})
+        cy.contains('label>span', 'search').parents('label').next('div').find('input').type('Hair Cut')
+        cy.contains('div', 'Hair Cut').parents('li').find('button').click({force: true})
+        cy.get('div[role="presentation"]').click({force: true}).type('{esc}')
+        //cy.get('div[data-dashlane-classification="login,step,final"]').find('ul>li').first().find('button').click({force: true})
+        cy.wait(100000)
         // cy.contains('button','Submit').click({force: true})
         // cy.contains('div>span','Start time cannot be empty').should('be.visible')
     })
