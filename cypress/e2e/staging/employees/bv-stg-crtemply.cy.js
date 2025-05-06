6c01abf517b7e1c99737d2826033a99c40c9cda2
Erika Maria Tavera Misas <dugarerika@gmail.com>
2025-05-06 09:03:09 +0300

    employee test cases fixes

--- cypress/e2e/staging/employees/bv-stg-crtemply.cy.js
+++ cypress/e2e/staging/employees/bv-stg-crtemply.cy.js
@@ -148,7 +148,7 @@ describe('Beta Vendor Admin | Employee | Create Employee| logged with Admin cred
         cy.expectedMessageCreateEmployee('password must be longer than or equal to 5 characters')
     })
 
-    it.only('Verify it is not possible to Add an Employee by filling up only the First Name, and Password | Username and Permission Level are required|', () => {
+    it('Verify it is not possible to Add an Employee by filling up only the First Name, and Password | Username and Permission Level are required|', () => {
         employeeSection()
         cy.contains('h6','employees', { matchCase: false }).parent().next('div').find('button').eq(1).should('exist')
         cy.contains('h6','employees', { matchCase: false }).parent().next('div').find('button').eq(1).click({ force: true })
